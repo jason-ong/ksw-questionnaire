c47661f7329048e61b746bbd59d5abdf56b2dc6e
Jason Ong <jasonong91@gmail.com>
2020-03-18 16:41:51 +0800

    Updates

---
+++ precache-manifest.3b7ec10a6dc64c7b46d3099333a93b9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/ksw-questionnaire/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/ksw-questionnaire/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "37ec7408f9adedc070f3",
+    "url": "/ksw-questionnaire/static/js/runtime~main.37ec7408.js"
+  },
+  {
+    "revision": "3375b2c805f6cd631934",
+    "url": "/ksw-questionnaire/static/js/main.3375b2c8.chunk.js"
+  },
+  {
+    "revision": "0105a9d721aef286c473",
+    "url": "/ksw-questionnaire/static/js/1.0105a9d7.chunk.js"
+  },
+  {
+    "revision": "3375b2c805f6cd631934",
+    "url": "/ksw-questionnaire/static/css/main.f8019fab.chunk.css"
+  },
+  {
+    "revision": "c6cbe724da7990e2745eda870a649d27",
+    "url": "/ksw-questionnaire/index.html"
+  }
+];
